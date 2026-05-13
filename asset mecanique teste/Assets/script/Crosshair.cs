a43b6e4512f11e08ec69c052b40c99c679ac3515
Natero08 <etan.vdk@gmail.com>
2026-05-13 02:21:35 +0200

    ui cleen and road fix
    Co-Authored-By: AlexX <273134248+Al3xX41@users.noreply.github.com>

--- asset mecanique teste/Assets/script/Crosshair.cs
+++ asset mecanique teste/Assets/script/Crosshair.cs
@@ -6,9 +6,16 @@ public class Crosshair : MonoBehaviour
     [SerializeField] private Image crosshairImage;
     [SerializeField] private Color defaultColor = Color.white;
     [SerializeField] private Color interactColor = Color.yellow;
+    [SerializeField] private Sprite defaultSprite;
+    [SerializeField] private Sprite interactSprite;
 
     public void SetInteractable(bool canInteract)
     {
         crosshairImage.color = canInteract ? interactColor : defaultColor;
+
+        if (canInteract && interactSprite != null)
+            crosshairImage.sprite = interactSprite;
+        else if (!canInteract && defaultSprite != null)
+            crosshairImage.sprite = defaultSprite;
     }
 }
